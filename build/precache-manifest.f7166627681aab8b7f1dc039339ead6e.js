6cc80ff9bd209edf5192839e174a36f857de8762
Vikas Dubey <vikasdubeysu9090@gmail.com>
2021-05-06 09:01:03 +0530

    build version

---
+++ build/precache-manifest.f7166627681aab8b7f1dc039339ead6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03aecae5d36f29306a7da36bd492c6ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e646bcf8d22cc26ce97e",
+    "url": "/static/css/main.167b4133.chunk.css"
+  },
+  {
+    "revision": "8facb3345d3a8680a8e8",
+    "url": "/static/js/2.49e91353.chunk.js"
+  },
+  {
+    "revision": "3493125ba862a8e2c7d04aaec8baffd8",
+    "url": "/static/js/2.49e91353.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e646bcf8d22cc26ce97e",
+    "url": "/static/js/main.7aee3fff.chunk.js"
+  },
+  {
+    "revision": "fa430200eae998bbeca5",
+    "url": "/static/js/runtime-main.ee4f1273.js"
+  }
+]);
